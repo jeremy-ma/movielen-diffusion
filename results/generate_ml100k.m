06f82e132cae07bcf2f447c8db7bf46d41047139
jeremy-ma <jeremy_ma@outlook.com>
2015-04-23 16:00:49 +1000

    added script to precompute the ml100k diffusion matrix

--- results/generate_ml100k.m
+++ results/generate_ml100k.m
@@ -2,13 +2,23 @@
 
 alpha_L = 5.5377e-07;
 alpha_nL =  0.0137;
+
+
+
 load('ml100k_difference.mat');
 ml100k_sim = to_similarity(ml100k_difference,0);
 %get rid of the infinities
-ml100k_sim(isinf(ml100k_sim)) = 100;
+ml100k_sim(isinf(ml100k_sim)) = 1000;
 %dodgy but needed infinities arise from two movies being identical
 ml100k_L = laplacian_matrix(ml100k_sim,'unnormalized');
 ml100k_L_norm = laplacian_matrix(ml100k_sim,'normalized');
+
+ratio_diagL_diagNL = sum(diag(ml100k_L)) / sum(diag(ml100k_L_norm));
+
+alpha_nL = 1;
+alpha_L = alpha_nL / ratio_diagL_diagNL;
+
+
 ml100k_diffusion = diffusion_matrix(ml100k_L,alpha_L);
 ml100k_diffusion_norm = diffusion_matrix(ml100k_L_norm,alpha_nL);
 
